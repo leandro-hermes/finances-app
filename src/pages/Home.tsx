17736962613a98e6a425bec32b761cb138ef2641
Leandro Hermes <leandro@leandrohermes.net>
2024-01-26 19:01:41 -0300

    feat(ExpenseForm): add autocomplete input for category

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,7 @@
+import CategoryIcon from "@mui/icons-material/Category";
 import SaveIcon from "@mui/icons-material/Save";
+import Autocomplete from "@mui/material/Autocomplete";
+import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
@@ -8,13 +11,14 @@ import { useApp } from "../hooks/app";
 import { CurrencyInput } from "../shared/components/CurrencyInput";
 import { now } from "../shared/util/dates.ts";
 
-type ExpenseFormValues = Record<'amount' | 'date' | 'description' | 'details', string>;
+type ExpenseFormValues = Record<"amount" | "date" | "description" | "details" | "category", string>;
 
 function extractFormData(form: HTMLFormElement) {
   const formData = new FormData(form);
   const data = Object.fromEntries(formData.entries()) as unknown as ExpenseFormValues;
 
   return {
+    ...data,
     amount: Number(data.amount.replace(/,/g, "")),
     date: data.date.split("/").reverse().join("-"),
     description: data.description || null,
@@ -56,6 +60,22 @@ const ExpenseForm = () => {
         />
         <TextField label="Descrição" name="description" variant="outlined" />
         <TextField label="Detalhes" name="details" variant="outlined" multiline />
+        <Autocomplete
+          disablePortal
+          options={[
+            "Mercado",
+            "Lazer/iFood",
+            "Farmácia",
+            "Combustível",
+          ]}
+          renderOption={(props, option) => (
+            <Box component="li" {...props}>
+              <CategoryIcon className="mr-2" />
+              {option}
+            </Box>
+          )}
+          renderInput={(params) => <TextField {...params} name="category" label="Categoria" required />}
+        />
       </Stack>
       <div className="absolute bottom-4 right-4">
         <Fab type="submit" color="primary" aria-label="add">

6b4e4bb421b9805590d25ff0af95f34233ca6da8
Leandro Hermes <leandro@leandrohermes.net>
2024-01-27 18:10:54 -0300

    chore: add pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+    })
+  ],
   resolve: {
     alias: {
-      '~context': '/src/app/context',
-      '~hooks': '/src/app/hooks',
-      '~pages': '/src/app/pages',
-      '~shared': '/src/app/shared',
+      "~context": "/src/app/context",
+      "~hooks": "/src/app/hooks",
+      "~pages": "/src/app/pages",
+      "~shared": "/src/app/shared",
     },
   },
 })

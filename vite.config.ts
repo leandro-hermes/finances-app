a3633e8d3a047597026a967bc8e6d14f3016f4fc
Leandro Hermes <leandro@leandrohermes.net>
2024-01-28 23:35:25 -0300

    chore: add pwa icons generation

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,36 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+      },
+      includeManifestIcons: true,
+      manifest: {
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
     })
   ],
   resolve: {
